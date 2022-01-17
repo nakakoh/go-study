064639a50613e7333789683c27732b6014902882
Kohji Nakahata <1259591+nakakoh@users.noreply.github.com>
2022-01-17 11:34:44 +0900

    add fetchall exercise.

---
+++ fetchall-1-10/fetchall.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // ゴルーチンを開始
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // ch チャネルから受信
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // ch チャネルへ送信
+		return
+	}
+
+	t := time.Now().Format("20060101_150405.000")
+	f, err := os.Create(t + ".log")
+	if err != nil {
+		ch <- fmt.Sprintf("fail to create file: %v", err)
+		return
+	}
+	nbytes, err := io.Copy(f, resp.Body)
+	resp.Body.Close() // 資源をリークさせない
+	f.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
