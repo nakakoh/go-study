4bcc878786be839288d5ff9cc8b1f42f0d1b052c
Kohji Nakahata <1259591+nakakoh@users.noreply.github.com>
2022-03-26 11:19:18 +0900

    tweak

--- ch11/storage1/storage.go
+++ ch11/storage1/storage.go
@@ -17,8 +17,8 @@ const hostname = "smtp.example.com"
 const template = `Warning: you are using %d bytes of storage, %d%% of your quota.`
 
 var notifyUser = func(username, msg string) {
-	auth := smtp.PlainAuth("", sender, password, hostname){
-	err := smtp.SendMail(hostnam+":587", auth, sender, []string{username}, []byte(msg))
+	auth := smtp.PlainAuth("", sender, password, hostname)
+	err := smtp.SendMail(hostname+":587", auth, sender, []string{username}, []byte(msg))
 	if err != nil {
 		log.Printf("smtp.SendEmail(%s) failed: %s", username, err)
 	}
